dd6532bb61dfde941d7990c90484a07912ffcd6e
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2022-03-13 23:12:11 -0300

    chore: add modal package

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import Login from './src/pages/Login';
 import Profile from './src/pages/Profile';
 import Settings from './src/pages/Settings';
